642c3dd99c7c4b10f399bca6710d6681da520b6c
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-06-30 23:21:43 -0700

    Time: 162 ms (30.68%), Space: 44.5 MB (57.29%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    
+    let leftSum = 0;
+    let rightSum = nums.reduce((acc, num) => acc + num, 0);
+    
+    for (let i = 0; i < nums.length; i++) {
+        rightSum -= nums[i]
+        if (leftSum === rightSum) {
+            return i;
+        }
+        leftSum += nums[i];
+    }
+    
+    return -1;
+    
+    
+    
+};
