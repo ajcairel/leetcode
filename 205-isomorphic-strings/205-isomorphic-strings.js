bd5c8f4d488d03cd63cfdefe0d0c49ff9dc12b9e
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-07-01 15:56:48 -0700

    Time: 74 ms (95.42%), Space: 48 MB (12.20%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -4,25 +4,13 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t){
-
-  const map = new Map();
-  const set = new Set();
-
-  for (let i = 0; i < s.length; i++) {
-    char1 = s.charAt(i);
-    char2 = t.charAt(i);
-      
-    if (map.has(char1) == true) {
-      if (map.get(char1) !== char2) {
-        return false;
-      }
-    } else {
-      if (set.has(char2)) {
-        return false;
-      }
-      map.set(char1, char2);
-      set.add(char2);
+    let arr1 = [];
+    let arr2 = [];
+    
+    for (let i = 0; i < s.length; i++){
+        arr1.push(s.indexOf(s[i]));
+        arr2.push(t.indexOf(t[i]));
     }
-  }
-  return true;
+    
+    return arr1.toString() === arr2.toString();
 }
