a1571c1f2a569589f2145108226c7d308e0b277e
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-07-05 11:54:36 -0700

    Time: 100 ms (65.84%), Space: 47.8 MB (12.63%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -4,13 +4,17 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t){
-    let arr1 = [];
-    let arr2 = [];
     
-    for (let i = 0; i < s.length; i++){
+    arr1 = [];
+    arr2 = [];
+    
+    for (let i = 0; i < s.length; i++) {
         arr1.push(s.indexOf(s[i]));
         arr2.push(t.indexOf(t[i]));
     }
     
+    
     return arr1.toString() === arr2.toString();
+
+  
 }
