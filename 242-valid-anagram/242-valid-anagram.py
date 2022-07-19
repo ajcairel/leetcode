f898d8b706bb92394ae8c56ab84674dac080fe67
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-07-19 16:36:53 -0700

    Time: 70 ms (60.84%), Space: 14.5 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,25 +1,50 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        counter = {}
-            
+        
+        # return Counter(s) == Counter(t) 
+        # return sorted(s) == sorted(t)
+        
+        # counter = {}
+        
         # if len(s) != len(t):
         #     return False
         
-        for char in s:
-            if char not in counter:
-                counter[char] = 1
-            else:
-                counter[char] += 1
+#         for char in s:
+#             if char not in counter:
+#                 counter[char] = 1
+#             else:
+#                 counter[char] += 1
         
-        for char in t:
-            if char in counter:
-                counter[char] -= 1
-            else:
-                return False
+#         for char in t:
+#             if char in counter:
+#                 counter[char] -= 1
+#             else:
+#                 return False
             
-        for val in counter.values():
-            if val != 0:
+#         for val in counter.values():
+#             if val != 0:
+#                 return False
+        
+#         return True
+
+        countS, countT = {}, {} 
+    
+        if len(s) != len(t):
+            return False
+    
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for char in countS:
+            if countS[char] != countT.get(char, 0):
                 return False
         
         return True
+    
+    # O(s + t)
+            
+            
         
+                 
+
+      
