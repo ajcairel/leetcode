ac27378e1456fb18c4612ba03b85c2fd7705e1e1
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-08-26 16:06:50 -0700

    Time: 80 ms (46.70%), Space: 14.5 MB (35.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -4,42 +4,43 @@ class Solution:
         # return Counter(s) == Counter(t) 
         # return sorted(s) == sorted(t)
         
-        # counter = {}
+        counter = {}
         
-        # if len(s) != len(t):
-        #     return False
-        
-#         for char in s:
-#             if char not in counter:
-#                 counter[char] = 1
-#             else:
-#                 counter[char] += 1
-        
-#         for char in t:
-#             if char in counter:
-#                 counter[char] -= 1
-#             else:
-#                 return False
-            
-#         for val in counter.values():
-#             if val != 0:
-#                 return False
-        
-#         return True
-
-        countS, countT = {}, {} 
-    
         if len(s) != len(t):
             return False
-    
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        for char in countS:
-            if countS[char] != countT.get(char, 0):
+        
+        for char in s:
+            if char not in counter:
+                counter[char] = 1
+            else:
+                counter[char] += 1
+        
+        for char in t:
+            if char in counter:
+                counter[char] -= 1
+            else:
+                return False
+            
+        for val in counter.values():
+            if val != 0:
                 return False
         
         return True
+
+#         countS, countT = {}, {} 
+    
+#         if len(s) != len(t):
+#             return False
+    
+#     # get handles key error
+#         for i in range(len(s)):
+#             countS[s[i]] = 1 + countS.get(s[i], 0)
+#             countT[t[i]] = 1 + countT.get(t[i], 0)
+#         for char in countS:
+#             if countS[char] != countT.get(char, 0):
+#                 return False
+        
+#         return True
     
     # O(s + t)
             
