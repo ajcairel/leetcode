a08f4ae42575d06e8e1506080d6e052b53815ce9
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-08-26 12:45:35 -0700

    Time: 2702 ms (5.01%), Space: 25 MB (38.55%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -5,11 +5,18 @@ class Solution:
         max_profit = 0
         while right < len(prices):
             current_profit = prices[right] - prices[left]
-            if prices[left] < prices[right]:
+            if prices[right] > prices[left]:
                 max_profit = max(current_profit, max_profit)
-            else: 
-                left = right
+            else:
+                left = right 
             right += 1
+            
         return max_profit
+    
+    
+    # if left is more than right, then left becomes right 
+    # Input = [7,2,5,3,6,4,1,7]
+    # Output = 6 (7 - 1)
+    # 2 was the lowest until it hit 1 
             
         
