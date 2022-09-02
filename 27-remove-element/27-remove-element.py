e3ce783cfc1d0d40b46843f20738ecdba4b5393c
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-09-02 12:33:19 -0700

    Time: 70 ms (10.62%), Space: 13.9 MB (63.05%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -6,4 +6,12 @@ class Solution:
                 nums[x] = num
                 x += 1
         return x
-                
+
+    
+    # return the number in place, so you only increment down the line if it is NOT the value to be removed
+    # [3,2,2,3] remove: 3
+    # x = 0, 3 is what is to be removed, it's at index 0
+    # when you get to 2, it is not to be removed, so it takes the place of 3, at index 0. 
+    # Increment x, x = 1
+    # Next in line is 2, not to be removed, so it takes index 1 spot. Increment x. x = 2
+    # The rest are 3's, so no more moves made, return x which is now 2
