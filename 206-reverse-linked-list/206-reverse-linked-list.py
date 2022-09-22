77ba970b4a2eafdafba11063cece5c34f99152e6
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-09-21 17:20:48 -0700

    Time: 57 ms (56.82%), Space: 15.5 MB (55.30%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -6,7 +6,7 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        prev = None
+        prev = None 
         cur = head
         
         while cur:
@@ -16,19 +16,4 @@ class Solution:
             cur = nxt
             
         return prev
-        
-        
-    
-        
-        #1 set prev to None and the current to head
-        
-        #2 you are going to reverse the list until cur = None 
-        
-        # think 'cascade'
-        
-        #3 set nxt to cur.next
-        #4 set cur.next to prev
-        #5 set prev to cur
-        #6 set cur to nxt
-        
-        #7 return prev 
+            
