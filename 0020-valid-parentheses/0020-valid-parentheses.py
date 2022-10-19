8dfbf79c00af283225b942448f6621702dc62711
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-10-19 11:45:17 -0700

    Time: 64 ms (25.79%), Space: 13.9 MB (72.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        parens = {'(':')', '[':']', '{':'}'}
+        stack = []
+        
+        for p in s:
+            if p in parens:
+                stack.append(p)
+            elif len(stack) == 0 or parens[stack.pop()] != p:
+                return False
+        
+        return len(stack) == 0
+        
+ 
+    
+   
+    
+	
+# 1. if it's the left bracket then we append it to the stack
+# 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
+# 3. finally check if the stack still contains unmatched left bracket
+        
+        
