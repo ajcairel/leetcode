afe1a6672f06a77f75881a269c5a88830d7529e8
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-10-26 14:51:18 -0700

    Time: 1935 ms (26.25%), Space: 15.4 MB (57.92%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,9 +1,35 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         
+#         if "0000" in deadends:
+#             return -1
+        
+        
+#         def children(lock):
+#             res = []
+#             for i in range(4):
+#                 digit = str((int(lock[i]) + 1) % 10)
+#                 res.append(lock[:i] + digit + lock[i+1:])
+#                 digit = str((int(lock[i]) - 1 + 10) % 10)
+#                 res.append(lock[:i] + digit + lock[i+1:])
+#             return res
+        
+#         q = deque()
+#         q.append(["0000", 0]) # [lock, turns]
+#         visit = set(deadends)
+#         while q:
+#             lock, turns = q.popleft()
+#             if lock == target:
+#                 return turns
+#             for child in children(lock):
+#                 if child not in visit:
+#                     visit.add(child)
+#                     q.append([child, turns + 1])
+        
+#         return -1
+    
         if "0000" in deadends:
             return -1
-        
         
         def children(lock):
             res = []
@@ -12,18 +38,22 @@ class Solution:
                 res.append(lock[:i] + digit + lock[i+1:])
                 digit = str((int(lock[i]) - 1 + 10) % 10)
                 res.append(lock[:i] + digit + lock[i+1:])
+            
             return res
-        
+            
+            
         q = deque()
-        q.append(["0000", 0]) # [lock, turns]
-        visit = set(deadends)
+        q.append(["0000", 0]) #lock, turn
+        visits = set(deadends)
         while q:
             lock, turns = q.popleft()
             if lock == target:
                 return turns
             for child in children(lock):
-                if child not in visit:
-                    visit.add(child)
+                if child not in visits:
+                    visits.add(child)
                     q.append([child, turns + 1])
         
-        return -1
+        return - 1
+    
+    
