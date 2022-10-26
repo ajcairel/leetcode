31105520dad60a86f1cb97f9ec693996c6695cf6
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-10-26 15:55:00 -0700

    Time: 990 ms (73.90%), Space: 15.3 MB (57.92%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,35 +1,9 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         
-#         if "0000" in deadends:
-#             return -1
-        
-        
-#         def children(lock):
-#             res = []
-#             for i in range(4):
-#                 digit = str((int(lock[i]) + 1) % 10)
-#                 res.append(lock[:i] + digit + lock[i+1:])
-#                 digit = str((int(lock[i]) - 1 + 10) % 10)
-#                 res.append(lock[:i] + digit + lock[i+1:])
-#             return res
-        
-#         q = deque()
-#         q.append(["0000", 0]) # [lock, turns]
-#         visit = set(deadends)
-#         while q:
-#             lock, turns = q.popleft()
-#             if lock == target:
-#                 return turns
-#             for child in children(lock):
-#                 if child not in visit:
-#                     visit.add(child)
-#                     q.append([child, turns + 1])
-        
-#         return -1
-    
         if "0000" in deadends:
             return -1
+        
         
         def children(lock):
             res = []
@@ -38,13 +12,12 @@ class Solution:
                 res.append(lock[:i] + digit + lock[i+1:])
                 digit = str((int(lock[i]) - 1 + 10) % 10)
                 res.append(lock[:i] + digit + lock[i+1:])
-            
             return res
-            
-            
+        
         q = deque()
-        q.append(["0000", 0]) #lock, turn
+        q.append(["0000", 0]) #lock, turns
         visits = set(deadends)
+        
         while q:
             lock, turns = q.popleft()
             if lock == target:
@@ -53,7 +26,46 @@ class Solution:
                 if child not in visits:
                     visits.add(child)
                     q.append([child, turns + 1])
+                    
+        return -1
+
+    
         
-        return - 1
+    
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+                            
+                            
+                            
     
     
