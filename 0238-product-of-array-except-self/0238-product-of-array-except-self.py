01a3fb4fb91a903c7222f289d4f6231f17338724
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2022-11-01 12:26:24 -0700

    Time: 237 ms (94.53%), Space: 21 MB (95.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,28 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        #Init array with 1's for every number in nums
         ans = [1 for _ in nums]
-      
         
-        #Two Pointers
         left = 1
         right = 1
-      
         
-        #loop over nums array
-        #[~i] takes the 'other end' of i
-        #goes from left to right until over
         for i in range(len(nums)):
             ans[i] *= left
-            ans[~i] *= right 
+            ans[~i] *= right
             left *= nums[i]
             right *= nums[~i]
             
-        return ans
-      
         
-    
+        return ans
+        
+      
         
     
     # Fill answer with 1's for every num in nums
