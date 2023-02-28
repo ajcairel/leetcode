ec6ab641cc23feb3c6ce8cb053f169d02eafa907
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-02-27 20:09:32 -0800

    Time: 59 ms (84.46%), Space: 14 MB (90.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,24 +1,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = {}
-        left = 0
+        
+        
+        used = {}
         maxLength = 0
+        start = 0
         
-        for i in range(len(s)):
-# If s[i] not in seen, we can keep increasing the window size by moving the right pointer
-            if s[i] not in seen:
-                maxLength = max(maxLength, i - left + 1)
-            
-# There are tow cases if s[i] is seen
-# 1. s[i] is inside the current window, we need to change the window by moving left pointer to seen[s[i]] + 1
-# 2. s[i] is NOT inside the current window, we can keep increasing the window
+        for index, char in enumerate(s):
+            # print(used)
+            # print(start)
+            if char in used and start <= used[char]:
+                start = used[char] + 1
             else:
-                if seen[s[i]] < left:
-                    maxLength = max(maxLength, i - left + 1)
-                else:
-                    left = seen[s[i]] + 1
-            seen[s[i]] = i
-        
+                maxLength = max(maxLength, index - start + 1 )
+                
+            used[char] = index
+            
         return maxLength
+        
+     
         
      
