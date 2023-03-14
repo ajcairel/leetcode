af3a054d2fe3d8cb8f008f66e4d166c53579da95
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-03-14 14:45:51 -0700

    Time: 86 ms (50.51%), Space: 17.7 MB (5.70%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,20 +1,9 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
-        # start, end = newInterval[0], newInterval[1]
-        # left, right = [], []
-        # for i in intervals:
-        #     if i[1] < start:
-        #         left += i,
-        #     elif i[0] > end:
-        #         right += i,
-        #     else:
-        #         start = min(start, i[0])
-        #         end = max(end, i[1])
-        # return left + [[start, end]] + right
-        
         start, end = newInterval[0], newInterval[1]
         left, right = [], []
+        
         for i in intervals:
             if i[1] < start:
                 left += i,
@@ -23,7 +12,10 @@ class Solution:
             else:
                 start = min(start, i[0])
                 end = max(end, i[1])
-        return left + [[start, end]] + right 
+        
+        return left + [[start, end]] + right
+        
+      
     
      
         
