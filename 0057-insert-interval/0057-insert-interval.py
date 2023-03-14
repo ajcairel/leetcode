0a89e7bb4109d5bdbfd6518edd2b742c5baa863e
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-03-13 21:11:23 -0700

    Time: 152 ms (5.10%), Space: 17.5 MB (15.30%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -4,8 +4,10 @@ class Solution:
         s, e = newInterval[0], newInterval[1]
         left, right = [], []
         for i in intervals:
-            print(i[1])
-            print(i[0])
+            print('i[0]: ', i[0])
+            print('i[1]: ', i[1])
+            print('s: ', s)
+            print('e: ', e)
             if i[1] < s:
                 left += i,
             elif i[0] > e:
@@ -13,6 +15,12 @@ class Solution:
             else:
                 s = min(s, i[0])
                 e = max(e, i[1])
+            print('interval: ', i)
+            print('left: ', left)
+            print('right: ', right)
+            print('s: ', s)
+            print('e: ', e)
+            print('-----')
         return left + [[s, e]] + right
 
       
