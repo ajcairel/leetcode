864a9f6dc045acc908853367521a9c54162b539c
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-03-13 21:24:44 -0700

    Time: 91 ms (25.49%), Space: 17.5 MB (24.14%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,6 +1,18 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
+        start, end = newInterval[0], newInterval[1]
+        left, right = [], []
+        for i in intervals:
+            if i[1] < start:
+                left += i,
+            elif i[0] > end:
+                right += i,
+            else:
+                start = min(start, i[0])
+                end = max(end, i[1])
+        return left + [[start, end]] + right
+        
 #         start, end = newInterval[0], newInterval[1]
 #         left, right = [], []
     
@@ -15,15 +27,15 @@ class Solution:
         
 #         return left + [[start, end]] + right 
     
-        s, e = newInterval[0], newInterval[1]
-        left, right = [], []
-        for i in intervals:
-            if i[1] < s:
-                left += i,
-            elif i[0] > e:
-                right += i,
-            else:
-                s = min(s, i[0])
-                e = max(e, i[1])
-        return left + [[s, e]] + right
+        # s, e = newInterval[0], newInterval[1]
+        # left, right = [], []
+        # for i in intervals:
+        #     if i[1] < s:
+        #         left += i,
+        #     elif i[0] > e:
+        #         right += i,
+        #     else:
+        #         s = min(s, i[0])
+        #         e = max(e, i[1])
+        # return left + [[s, e]] + right
         
