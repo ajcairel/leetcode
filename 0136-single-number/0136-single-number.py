00e9fd94c1a65dfaf68273ba6ae46868e476a180
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-03-21 10:46:06 -0700

    Time: 148 ms (33.93%), Space: 16.8 MB (44.28%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,23 +1,35 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
+        # using Counter 
+        
 #         chars = Counter(nums)
         
 #         for num in nums:
 #             if chars[num] == 1:
 #                 return num
-#         counts = {}
-    
-#         for num in nums:
-#             counts[num] = counts.get(num, 0) + 1
-        
-#         for n in counts:
-#             if counts[n] == 1:
-#                 return n
-        xor = 0
+
+# Using a dictionary 
+        counts = {}
     
         for num in nums:
-            xor ^= num
+            counts[num] = counts.get(num, 0) + 1
+            
+        for key, val in counts.items():
+            if val == 1:
+                return key
+
+            
+                
         
-        return xor
+      
+# Bit manipulation 
+#         xor = 0
+    
+#         for num in nums:
+#             xor ^= num
+#             print(xor)
+        
+#         return xor
+
         
