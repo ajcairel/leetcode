2393e253166f26f4bad91c36e21b8e3a8f0f758e
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-03-21 20:07:40 -0700

    Time: 43 ms (14.83%), Space: 13.9 MB (54.93%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+
+
+        chars = []
+        
+        for c in s:
+            if c not in chars:
+                chars.append(c)
+            else:
+                chars.remove(c)
+        
+        return len(s) - len(chars) + 1 if len(chars) > 0 else len(s)
+    
+ 
+
+    
+    
+    
+    # check every char in s
+    # if it's not in hash, append it
+    # if it is, remove it
+    # return the length of s - length of hash + 1
+    # if the hash has things in it. This would b
