0bfe4f34157e8bb901252374d26c3903ab13abab
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-04-06 13:31:30 -0700

    Time: 61 ms (92.78%), Space: 14.4 MB (10.50%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -8,16 +8,15 @@ class Solution:
                 stack.append(int(t))
             else:
                 r, l = stack.pop(), stack.pop()
-                
                 if t == "+":
                     stack.append(l + r)
                 elif t == "-":
                     stack.append(l - r)
-                elif t == "*":
+                elif t == '*':
                     stack.append(l * r)
                 else:
                     stack.append(int(float(l / r)))
         
         return stack.pop()
-            
-  
+    
+        
