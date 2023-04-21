35bbf6a8c7fbc67151ccba9d845a023fb13d8eb2
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-04-21 16:58:49 -0700

    Time: 139 ms (94.81%), Space: 18.9 MB (20.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,7 +2,6 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
         intervals.sort(key = lambda i: i[0])
-        
         output = [intervals[0]]
         
         for start, end in intervals[1:]:
@@ -10,8 +9,9 @@ class Solution:
                 output[-1][1] = max(output[-1][1], end)
             else:
                 output.append([start, end])
-        
+                
         return output
+       
         
        
         
