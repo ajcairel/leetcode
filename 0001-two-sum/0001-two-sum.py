9106660b8c150887331506b55cf7a52704ea9076
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2023-09-07 17:04:47 -0700

    Time: 67 ms (68.78%), Space: 17.9 MB (8.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -7,6 +7,8 @@ class Solution:
             if num in pairs:
                 return [pairs[num], index]
             else:
-                pairs[target - num] = index 
-
-       
+                pairs[target - num] = index
+                
+            
+        
+     
