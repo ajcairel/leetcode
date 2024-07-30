29e0e5dfba937b171be868088360f1eba9f79c3f
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2024-07-30 14:25:17 -0700

    Time: 40 ms (37.23%), Space: 16.5 MB (16.83%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -10,12 +10,13 @@ class Solution:
         
         while mid <= right:
             if nums[mid] == 0:
-                nums[mid], nums[left] = nums[left], nums[mid]
+                nums[left], nums[mid] = nums[mid], nums[left]
+                left += 1
                 mid += 1
-                left +=1
             elif nums[mid] == 1:
                 mid += 1
             else:
-                nums[mid], nums[right] = nums[right], nums[mid]
-                right -=1
-    
+                nums[right], nums[mid] = nums[mid], nums[right]
+                right -= 1
+                
+                
