5631697b2987ef43518a8390a25592083caffba9
Alexander Cairel <100811422+ajcairel@users.noreply.github.com>
2024-08-13 20:06:59 -0700

    Time: 363 ms (82.46%), Space: 17 MB (80.72%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,30 +1,20 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-         # Build frequency map
-        freq = [0] * 26
-        for ch in tasks:
-            freq[ord(ch) - ord('A')] += 1
+        taskMap = {}
+        # Simply make hashmap which is used for storing frequencies of tasks
+        for task in tasks:
+            taskMap[task] = taskMap.get(task, 0) + 1
+        # Sort it to find the task which occurs Maximum
+        maxList = sorted(taskMap.values(), reverse=True)
         
-        # Max heap to store frequencies
-        pq = [-f for f in freq if f > 0]
-        heapq.heapify(pq)
-
-        time = 0
-        # Process tasks until the heap is empty
-        while pq:
-            cycle = n + 1
-            store = []
-            task_count = 0
-            # Execute tasks in each cycle
-            while cycle > 0 and pq:
-                current_freq = -heapq.heappop(pq)
-                if current_freq > 1:
-                    store.append(-(current_freq - 1))
-                task_count += 1
-                cycle -= 1
-            # Restore updated frequencies to the heap
-            for x in store:
-                heapq.heappush(pq, x)
-            # Add time for the completed cycle
-            time += task_count if not pq else n + 1
-        return time
+        maxNum = maxList[0]
+        
+        i, maxTaskCounter = 0, 0
+        # Now find how many tasks are there which occur maximum number of times after we founded maxFrequency above
+        while i < len(maxList) and maxList[i] == maxNum:
+            maxTaskCounter += 1
+            i += 1
+        # The formula that we derived after taking different cases as aboove
+        intervals = (maxNum - 1) * (n + 1) + maxTaskCounter
+        # Finally the formula doesn'y hold for some cases like  (ABCABCDEFG) so we take the max of len(tasks) to get our final answer
+        return max(intervals, len(tasks))
